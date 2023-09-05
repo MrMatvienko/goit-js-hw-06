281ddf0a18dfddf9313335e7d9f7700590ffeec2
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2023-09-05 13:27:31 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorSpan = document.querySelector(".color");
+const changeButton = document.querySelector(".change-color");
+const body = document.body;
+
+changeButton.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
